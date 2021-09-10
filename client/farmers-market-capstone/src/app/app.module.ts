9f50cff9eba3c07155bf658bea63268e8f259ca8
jisae9d <mark.zemlany@jbhunt.com>
2021-09-10 16:02:42 -0500

    created home page and inital styling

--- client/farmers-market-capstone/src/app/app.module.ts
+++ client/farmers-market-capstone/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
